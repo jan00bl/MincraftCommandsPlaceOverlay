5b8fb59565d848327b4613d0c753134fd32d173d
jan00bl <39634838+jan00bl@users.noreply.github.com>
2022-04-03 16:04:01 +0200

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -17,7 +17,7 @@ if (window.top !== window.self) {
         (function () {
             const i = document.createElement("img");
             i.src = "https://raw.githubusercontent.com/jan00bl/MincraftCommandsPlaceOverlay/77719f83fa697028238ab9579a9fa1406193ddb6/commands_place.png";
-            i.style = "position: absolute;left: 1.1px;top: 0px;image-rendering: pixelated;width: 2000px;height: 1000px;";
+            i.style = "position: absolute;left: 0px;top: 0px;image-rendering: pixelated;width: 2000px;height: 1000px;";
             console.log(i);
             return i;
         })())
