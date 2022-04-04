53088d1de4212d26c082457feaffb98c40b11d3a
jan00bl <39634838+jan00bl@users.noreply.github.com>
2022-04-04 15:16:58 +0200

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -2,13 +2,13 @@
 // ==UserScript==
 // @name         April Fools Minecraft Commands Allies
 // @namespace    http://tampermonkey.net/
-// @version      3.17
+// @version      3.18
 // @description  Keep the canvas beautiful!
 // @author       Adapted by u/jan00bl for Minecraft Commands, Original author oralekin from osu! /r/osuplace
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @updateURL    https://github.com/jan00bl/MincraftCommandsPlaceOverlay/raw/main/script.user.js
-// @downloadURL  https://github.com/jan00bl/MincraftCommandsPlaceOverlay/raw/main/script.user.js
+// @updateURL    https://github.com/jan00bl/MinecraftCommandsPlaceOverlay/raw/main/script.user.js
+// @downloadURL  https://github.com/jan00bl/MinecraftCommandsPlaceOverlay/raw/main/script.user.js
 // @grant        none
 // ==/UserScript==
 if (window.top !== window.self) {
@@ -16,7 +16,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://raw.githubusercontent.com/jan00bl/MincraftCommandsPlaceOverlay/710ee85319e4d0b3d3ec39f59533aed6b7e784f8/commands_place.png";
+            i.src = "https://raw.githubusercontent.com/jan00bl/MinecraftCommandsPlaceOverlay/d1ca53a0afd3ebc72def6358deaafb5bceb8b6af/commands_place.png";
             i.style = "position: absolute;left: 0px;top: 0px;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
